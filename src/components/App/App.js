934bac2458358838050501ee60a81ed331facf52
Ronald Head <ronaldlhead@gmail.com>
2022-03-24 14:44:49 -0600

    Import movie data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import movieData from '../../movies-data';
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      movies: [],
+      movies: [ {movieData} ],
       error: ''
     }
   }
