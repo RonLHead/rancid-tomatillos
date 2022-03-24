83deeeb784a5630164a90cd722c737ee3958c1e6
Ronald Head <ronaldlhead@gmail.com>
2022-03-24 15:22:17 -0600

    Set up/map movie data to movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
+import MovieCard from './src/components/MovieCard/MovieCard.js';
 import movieData from '../../movies-data';
 
-const Movies = ({movieData}) => {
-    
+const Movies = ( {movieData} ) => {
+    const movieCards = movieData.map(movie => {
+        return (
+            <MovieCard 
+                id={movie.id}
+                title={movie.title}
+                posterPath={movie.poster_path}
+            />
+        )
+    })
+
+    return (
+        <div>
+            {movieCards}
+        </div>
+    )
 }
 
 export default Movies
