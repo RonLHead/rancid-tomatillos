0d3f8bb96948f657fd9c5a1fadcd54b182d1b90f
Casey Halstead <catherineclosee@gmail.com>
2022-03-26 13:40:25 -0500

    Update for structure of the single view elements

--- src/components/CurrentMovie/CurrentMovie.js
+++ src/components/CurrentMovie/CurrentMovie.js
@@ -4,16 +4,24 @@ import './CurrentMovie.css'
 const CurrentMovie = ({currentMovie, displayAllMovies}) => {
     return (
         <div className="current-movie">
-            <img src={currentMovie.backdrop_path} alt="Backdrop image"></img>
-            <h3>{currentMovie.title}</h3>
-            <p>{currentMovie.tagline}</p>
-            <p>{currentMovie.overview}</p>
-            <p>{currentMovie.runtime} minutes</p>
-            <p>{currentMovie.release_date}</p>
-            <p>{currentMovie.genres}</p>
-            <p>Budget: ${currentMovie.budget}</p>
-            <p>Revenue: ${currentMovie.revenue}</p>
-            <p>{currentMovie.average_rating}</p>
+            <img className="backdrop" src={currentMovie.backdrop_path} alt="Backdrop image"></img>
+            <div className="title-container">
+                <div>
+                <img src={currentMovie.poster_path} alt="Poster image"></img>  
+                </div>
+                <div className="movie-info">
+                    <h3>{currentMovie.title}</h3>
+                    <p>{currentMovie.tagline}</p>
+                    <p>{currentMovie.release_date}, {currentMovie.genres}, {currentMovie.runtime}m</p>
+                    <p>🍅 Average Rating: {currentMovie.average_rating}</p>
+                </div>
+            </div>
+            <div className="facts-container">
+                <h3>WHAT TO KNOW</h3>
+                <p>{currentMovie.overview}</p>
+                <p>Budget: ${currentMovie.budget}</p>
+                <p>Revenue: ${currentMovie.revenue}</p>
+            </div>
             <button onClick={() => displayAllMovies()}>Back</button>
         </div>
     )
