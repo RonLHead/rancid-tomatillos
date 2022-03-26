9002af516e69d6663cb3c56b528cb200fd4dc0ad
Casey Halstead <catherineclosee@gmail.com>
2022-03-26 16:08:16 -0500

    Refactor html structure for single movie display

--- src/components/CurrentMovie/CurrentMovie.js
+++ src/components/CurrentMovie/CurrentMovie.js
@@ -6,21 +6,17 @@ const CurrentMovie = ({currentMovie, displayAllMovies}) => {
         <div className="current-movie">
             <img className="backdrop" src={currentMovie.backdrop_path} alt="Backdrop image"></img>
             <div className="title-container">
-                <div>
-                <img src={currentMovie.poster_path} alt="Poster image"></img>  
-                </div>
+                <img className="current-poster" src={currentMovie.poster_path} alt="Poster image"></img>  
                 <div className="movie-info">
                     <h3>{currentMovie.title}</h3>
                     <p>{currentMovie.tagline}</p>
                     <p>{currentMovie.release_date}, {currentMovie.genres}, {currentMovie.runtime}m</p>
                     <p>🍅 Average Rating: {currentMovie.average_rating}</p>
+                    <h3>WHAT TO KNOW</h3>
+                    <p>{currentMovie.overview}</p>
+                    <p>Budget: ${currentMovie.budget}</p>
+                    <p>Revenue: ${currentMovie.revenue}</p>
                 </div>
-            </div>
-            <div className="facts-container">
-                <h3>WHAT TO KNOW</h3>
-                <p>{currentMovie.overview}</p>
-                <p>Budget: ${currentMovie.budget}</p>
-                <p>Revenue: ${currentMovie.revenue}</p>
             </div>
             <button onClick={() => displayAllMovies()}>Back</button>
         </div>
