fb82bd755fb9ded7e128f15af2ffb65301a5b2bd
Ronald Head <ronaldlhead@gmail.com>
2022-03-28 16:39:38 -0600

    Adjust element sizing and background color in current movie view

--- src/components/CurrentMovie/CurrentMovie.js
+++ src/components/CurrentMovie/CurrentMovie.js
@@ -4,18 +4,24 @@ import './CurrentMovie.css'
 const CurrentMovie = ({currentMovie, displayAllMovies}) => {
     return (
         <div className="current-movie">
-            <img className="backdrop" src={currentMovie.backdrop_path} alt="Backdrop image"></img>
+            {/* <div> */}
+                <img className="backdrop" src={currentMovie.backdrop_path} alt="Backdrop image"></img>
+            {/* </div> */}
             <div className="title-container">
                 <img className="current-poster" src={currentMovie.poster_path} alt="Poster image"></img>  
                 <div className="movie-info">
-                    <h3>{currentMovie.title}</h3>
-                    <p>{currentMovie.tagline}</p>
-                    <p>{currentMovie.release_date}, {currentMovie.genres}, {currentMovie.runtime}m</p>
+                    <h3 className="current-movie-title">{currentMovie.title}</h3>
+                    <div className="current-movie-stats">
+                        <p>{currentMovie.tagline}</p>
+                        <p>{currentMovie.release_date}, {currentMovie.genres}, {currentMovie.runtime}m</p>
                     <p>🍅 Average Rating: {currentMovie.average_rating}</p>
-                    <h3>WHAT TO KNOW</h3>
-                    <p>{currentMovie.overview}</p>
-                    <p>Budget: ${currentMovie.budget}</p>
-                    <p>Revenue: ${currentMovie.revenue}</p>
+                    </div>
+                    <div className="current-movie-wtk">
+                        <h3>WHAT TO KNOW</h3>
+                        <p>{currentMovie.overview}</p>
+                        <p>Budget: ${currentMovie.budget}</p>
+                        <p>Revenue: ${currentMovie.revenue}</p>
+                    </div>
                 </div>
             </div>
             <button onClick={() => displayAllMovies()}>Back</button>
