6cc6b09b294ed344375dd0c34b55b61293ba5126
Casey Halstead <catherineclosee@gmail.com>
2022-03-30 14:15:56 -0500

    Refactored component to be a class component and refactored fetch call for currentMovie, and refactor render

--- src/components/CurrentMovie/CurrentMovie.js
+++ src/components/CurrentMovie/CurrentMovie.js
@@ -1,37 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
+import fetchMovies from '../../apiCalls.js';
 import './CurrentMovie.css'
+import { NavLink } from 'react-router-dom'
 
-const CurrentMovie = ({currentMovie, displayAllMovies}) => {
-    const movieYear = currentMovie.release_date.split('-');
-    const movieGenres = currentMovie.genres.join(', ');
+class CurrentMovie extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            currentMovie: '',
+            error: ''
+        }
+    } 
 
-    const avgRating = currentMovie.average_rating.toFixed(0);
+    fetchCurrentMovie = (id) => {
+        fetchMovies.getMovieData(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+            .then(data => this.setState({currentMovie: data.movie}))
+            .catch(error => {
+                console.log(error)
+                this.setState({error: `${error}`})
+            })
+    }
+    
+    componentDidMount = () => this.fetchCurrentMovie(this.props.id)
+    
+    render() {
+        let currentMovieDetails;
+        if(this.state.currentMovie) {
+            let movieYear = this.state.currentMovie.release_date.split('-')[0];
+            let movieGenres = this.state.currentMovie.genres.join(', ');
+            let movieRating = this.state.currentMovie.average_rating.toFixed(0);
+         
 
-    return (
-        <div className="current-movie">
-            {/* <div> */}
-                <img className="backdrop" src={currentMovie.backdrop_path} alt="Backdrop image"></img>
-            {/* </div> */}
-            <div className="title-container">
-                <img className="current-poster" src={currentMovie.poster_path} alt="Poster image"></img>  
-                <div className="movie-info">
-                    <h3 className="current-movie-title">{currentMovie.title}</h3>
-                    <div className="current-movie-stats">
-                        <p>{currentMovie.tagline}</p>
-                        <p>{movieYear[0]}, {movieGenres}, {currentMovie.runtime}m</p>
-                    <p>🍅 Average Rating: {avgRating}</p>
-                    </div>
-                    <div className="current-movie-wtk">
-                        <h3>WHAT TO KNOW</h3>
-                        <p>{currentMovie.overview}</p>
-                        <p>Budget: {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(currentMovie.budget)}</p>
-                        <p>Revenue: {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(currentMovie.revenue)}</p>
+        currentMovieDetails = (
+            <div className="current-movie">
+                <img className="backdrop" src={this.state.currentMovie.backdrop_path} alt="Backdrop image"></img>
+                <div className="title-container">
+                    <img className="current-poster" src={this.state.currentMovie.poster_path} alt="Poster image"></img>  
+                    <div className="movie-info">
+                        <h3 className="current-movie-title">{this.state.currentMovie.title}</h3>
+                        <div className="current-movie-stats">
+                            <p>{this.state.currentMovie.tagline}</p>
+                            <p>{movieYear}, {movieGenres}, {this.state.currentMovie.runtime}m</p>
+                            <p>🍅 Average Rating: {movieRating}</p>
+                        </div>
+                        <div className="current-movie-wtk">
+                            <h3>WHAT TO KNOW</h3>
+                            <p>{this.state.currentMovie.overview}</p>
+                            <p>Budget: {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(this.state.currentMovie.budget)}</p>
+                            <p>Revenue: {new Intl.NumberFormat("en-US", { style: "currency", currency: "USD" }).format(this.state.currentMovie.revenue)}</p>
+                        </div>
                     </div>
                 </div>
+                <NavLink to="/">
+                    <button>Back</button>
+                </NavLink>
             </div>
-            <button onClick={() => displayAllMovies()}>Back</button>
-        </div>
-    )
+        )
+
+        }
+        
+        return (
+            <div>
+                {currentMovieDetails}
+            </div>
+        )
+    }
 }
 
 export default CurrentMovie
