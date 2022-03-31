eba5f398fb3477083fd9a733f5bc8c20b14aac32
Ronald Head <ronaldlhead@gmail.com>
2022-03-31 15:27:24 -0600

    Set up intercept for movies flow happy path

--- cypress/integration/movies_spec.js
+++ cypress/integration/movies_spec.js
@@ -1,5 +1,8 @@
-describe('Movies homepage flow happy path', () => {
+import moviesTestData from './movies_test_data';
+
+describe('Movies homepage flow', () => {
     it('Should be able to visit the page and render the correct elements', () => {
+        cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', moviesTestData)
         cy.visit('http://localhost:3000')
             .contains('Rancid Tomatillos')
             .get('h2')
