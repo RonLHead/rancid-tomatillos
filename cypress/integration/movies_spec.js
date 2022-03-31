8ad3aed35c7cdba670375486b73819ebc266fcee
Ronald Head <ronaldlhead@gmail.com>
2022-03-31 17:10:29 -0600

    Set up sad path movies flow

--- cypress/integration/movies_spec.js
+++ cypress/integration/movies_spec.js
@@ -18,6 +18,20 @@ describe('Movies homepage flow', () => {
                     .should('include', '/718444')
         
     })
+
+    it("Should be able to display an error message if the movies data isn't fetched properly", () => {
+        cy.intercept('GET', 'https://rancid-tomatillos.herokuapp.com/api/v2/movies', {forceNetworkError: true})
+        cy.visit('http://localhost:3000')
+            .contains('Rancid Tomatillos')
+            .get('h2')
+                .should('have.class', 'sub-heading')
+                .contains('The second most trusted measurer of movie quality!')
+            .get('div')
+                .should('have.class', 'movies-container')
+                .contains('TypeError: Failed to fetch')
+                
+        
+    })
 })
 
 
