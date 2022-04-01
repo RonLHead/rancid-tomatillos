d2217a042285719bfb24e398883fbb90eaa40d77
Casey Halstead <catherineclosee@gmail.com>
2022-04-01 17:37:46 -0500

    Remove unused import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App.js';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const router = <React.StrictMode> <BrowserRouter> <App /> </BrowserRouter> </React.StrictMode>
