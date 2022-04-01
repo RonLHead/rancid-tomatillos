00c3adf5488cbd657f4d2cc09658e6cbbd20ca70
Casey Halstead <catherineclosee@gmail.com>
2022-04-01 17:35:39 -0500

    Remove unused function and code comments

--- src/index.js
+++ src/index.js
@@ -8,8 +8,3 @@ import { BrowserRouter } from 'react-router-dom';
 const router = <React.StrictMode> <BrowserRouter> <App /> </BrowserRouter> </React.StrictMode>
 
 ReactDOM.render(router, document.getElementById('root'));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
